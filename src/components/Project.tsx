755c3d051ec6fae7340c63259abf085681e87f31
Valentina Haddad <vhaddad@bu.edu>
2024-10-21 16:41:05 -0400

    went to oh today and figured out the error of why vercel and localhost look different, my serch engine automatically puts at 67% view while when you open in vercel it goes to 100%

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -22,39 +22,41 @@ const Project: React.FC = () => {
             <h1 id="pageWrapperHeading">Projects</h1>
 
             <div className="calculator-container">
+
                 <h2 className="CalculatorProjHeading">Cotton Candy Calculator</h2>
+                <div className="PageWrapperCalc">
+                    <label htmlFor="first-number">First Number:</label>
+                    <input
+                        id="first-number"
+                        type="number"
+                        value={num1}
+                        onChange={(e) => setNum1(e.target.value)}
+                    />
 
-                <label htmlFor="first-number">First Number:</label>
-                <input
-                    id="first-number"
-                    type="number"
-                    value={num1}
-                    onChange={(e) => setNum1(e.target.value)}
-                />
+                    <label htmlFor="second-number">Second Number:</label>
+                    <input
+                        id="second-number"
+                        type="number"
+                        value={num2}
+                        onChange={(e) => setNum2(e.target.value)}
+                    />
 
-                <label htmlFor="second-number">Second Number:</label>
-                <input
-                    id="second-number"
-                    type="number"
-                    value={num2}
-                    onChange={(e) => setNum2(e.target.value)}
-                />
+                    <div className="buttons">
+                        <button onClick={add}>+</button>
+                        <button onClick={sub}>-</button>
+                        <button onClick={multiplication}>×</button>
+                        <button onClick={division}>÷</button>
+                        <button onClick={power}>**</button>
+                        <button onClick={clearCalc}>Clear</button>
+                    </div>
 
-                <div className="buttons">
-                    <button onClick={add}>+</button>
-                    <button onClick={sub}>-</button>
-                    <button onClick={multiplication}>×</button>
-                    <button onClick={division}>÷</button>
-                    <button onClick={power}>**</button>
-                    <button onClick={clearCalc}>Clear</button>
-                </div>
-
-                <div id="output-box">
-                    <h3 id="output" style={{ color }}>{result}</h3>
+                    <div id="output-box">
+                        <h3 id="output" style={{color}}>{result}</h3>
+                    </div>
                 </div>
             </div>
         </div>
     );
-};
+            };
 
-export default Project;
+            export default Project;
