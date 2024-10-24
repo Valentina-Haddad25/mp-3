6a86be63be320d889f1f61888a4e879490f9d37d
Valentina Haddad <vhaddad@bu.edu>
2024-10-23 21:47:52 -0400

    final comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+// Import necessary modules from react-router-dom for routing functionalities
+
 import Navbar from './components/Navbar';
 import Education from './components/Education';
 import Project from './components/Project';
@@ -7,26 +9,37 @@ import Certifications from './components/Certifications';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Footer from './components/Footer';
+// Import the component files that will be displayed on different routes
 
 const App = () => {
     return (
         <BrowserRouter>
+            {/* BrowserRouter wraps the entire application to provide routing context */}
             <Routes>
-                <Route path="/" element={
-                    <>
-                        <Navbar />
-                        <div className="content">
-                            <Outlet />
-                        </div>
-                        <Footer />
-                    </>
-                }>
+                {/* Routes is the container for all Route components */}
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            {/* Display Navbar, content, and Footer on all routes */}
+                            <Navbar />
+                            <div className="content">
+                                {/* Outlet renders the matched child route’s component */}
+                                <Outlet />
+                            </div>
+                            <Footer />
+                        </>
+                    }
+                >
+                    {/* Define nested routes that will be rendered inside the Outlet */}
                     <Route path="home" element={<Home />} />
                     <Route path="education" element={<Education />} />
                     <Route path="employment" element={<Employment />} />
                     <Route path="project" element={<Project />} />
                     <Route path="certifications" element={<Certifications />} />
                     <Route path="contact" element={<Contact />} />
+
+                    {/* Set the default route to redirect to /home */}
                     <Route index element={<Navigate to="home" />} />
                 </Route>
             </Routes>
@@ -35,3 +48,4 @@ const App = () => {
 };
 
 export default App;
+// Export the App component to be used elsewhere in your project
